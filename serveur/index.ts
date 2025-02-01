9c466c188c9f95229db53442bc25b4b21790e9dd
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-02-01 10:00:25 +0100

    [CI/CD] Utilise `tsx` et la version de node 20
    ... pour éviter d'utiliser le package tierce `ts-node`

--- serveur/index.ts
+++ serveur/index.ts
@@ -1,8 +1,5 @@
 import express, {Express, Request, Response} from "express";
-import dotenv from "dotenv";
 import {ReponseAPIAlbert} from "./types";
-
-dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
