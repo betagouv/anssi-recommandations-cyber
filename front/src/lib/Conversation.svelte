f0a39b895d89304fe114c6a1e7b31f8c620b5619
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-02-01 10:49:55 +0100

    Affiche le score de confiance de chaque réponse

--- front/src/lib/Conversation.svelte
+++ front/src/lib/Conversation.svelte
@@ -7,6 +7,7 @@
         contenu: string;
         documentsAssocies?: string[];
         expediteur: 'Moi' | 'Serveur';
+        scoreMoyen?: number;
     }
 
     let messages: Message[] = [];
@@ -24,12 +25,17 @@
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ demande: contenu })
+            body: JSON.stringify({demande: contenu})
         })).json();
 
         const reponseAvecRecommandation = reponse.reponse.replaceAll(/(R\d+)/g, "**$1**");
 
-        messages = [...messages, {contenu: reponseAvecRecommandation, documentsAssocies: reponse.sources, expediteur: 'Serveur'}];
+        messages = [...messages, {
+            contenu: reponseAvecRecommandation,
+            documentsAssocies: reponse.sources,
+            scoreMoyen: reponse.scoreMoyen,
+            expediteur: 'Serveur'
+        }];
         enAttente = false;
     };
 </script>
@@ -38,8 +44,11 @@
     {#each messages as message, idx (idx)}
         {@const estUtilisateur = message.expediteur === 'Moi'}
         <div class="message" class:estUtilisateur>
+            {#if message.scoreMoyen}
+                <span class="score">Score : {(message.scoreMoyen * 100).toFixed(1)}%</span>
+            {/if}
             <p>
-                <SvelteMarkdown source={message.contenu} />
+                <SvelteMarkdown source={message.contenu}/>
                 {#if message.documentsAssocies}
                     {#each message.documentsAssocies as document}
                         <a href="/document/{document}">({document})</a>
@@ -51,12 +60,12 @@
 </div>
 {#if enAttente}
     <div class="conteneur-loader">
-        <div class="loader" />
+        <div class="loader"/>
     </div>
 {/if}
 <form on:submit|preventDefault={envoiDemande}>
     <input type="text" bind:value={demande} placeholder="Posez votre question !"/>
-    <button type="submit" />
+    <button type="submit"/>
 </form>
 
 <style>
@@ -75,6 +84,7 @@
         padding: 13px 11px 13px 32px;
         border-left: 4px solid #6A6AF4;
         margin-left: 20px;
+        position: relative;
     }
 
     .message p {
@@ -123,19 +133,38 @@
     .loader {
         width: 30px;
         aspect-ratio: 2;
-        --_g: no-repeat radial-gradient(circle closest-side,#6A6AF4 90%,#0000);
-        background:
-                var(--_g) 0%   50%,
-                var(--_g) 50%  50%,
-                var(--_g) 100% 50%;
-        background-size: calc(100%/3) 50%;
+        --_g: no-repeat radial-gradient(circle closest-side, #6A6AF4 90%, #0000);
+        background: var(--_g) 0% 50%,
+        var(--_g) 50% 50%,
+        var(--_g) 100% 50%;
+        background-size: calc(100% / 3) 50%;
         animation: l3 1s infinite linear;
     }
 
     @keyframes l3 {
-        20%{background-position:0%   0%, 50%  50%,100%  50%}
-        40%{background-position:0% 100%, 50%   0%,100%  50%}
-        60%{background-position:0%  50%, 50% 100%,100%   0%}
-        80%{background-position:0%  50%, 50%  50%,100% 100%}
+        20% {
+            background-position: 0% 0%, 50% 50%, 100% 50%
+        }
+        40% {
+            background-position: 0% 100%, 50% 0%, 100% 50%
+        }
+        60% {
+            background-position: 0% 50%, 50% 100%, 100% 0%
+        }
+        80% {
+            background-position: 0% 50%, 50% 50%, 100% 100%
+        }
+    }
+
+    .score {
+        position: absolute;
+        top: 0;
+        left: 32px;
+        font-size: 0.7rem;
+        background: #6A6AF4;
+        padding: 2px 4px;
+        color: white;
+        font-weight: bold;
+        border-radius: 4px;
     }
 </style>
