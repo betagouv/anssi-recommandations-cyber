f11b73a57abf80833973eec2bab4b6538faa441a
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-02-04 17:07:52 +0100

    Affiche correctement l'url et le nom des documents

---
+++ front/src/lib/documents.ts
@@ -0,0 +1,47 @@
+type Document = {
+    url: string;
+    label: string;
+}
+
+export const documents: Record<string, Document> = {
+    "anssi_amrae-guide-maitrise_risque_numerique-atout_confiance.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2019/11/anssi_amrae-guide-maitrise_risque_numerique-atout_confiance.pdf",
+        label: "MAÎTRISE DU RISQUE NUMÉRIQUE"
+    },
+    "guide-cartographie-systeme-information-anssi-pa-046.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2018/11/guide-cartographie-systeme-information-anssi-pa-046.pdf",
+        label: "CARTOGRAPHIE DU SYSTÈME D’INFORMATION"
+    },
+    "anssi-guide-communication_crise_cyber.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2021/12/anssi-guide-communication_crise_cyber.pdf",
+        label: "ANTICIPER ET GÉRER SA COMMUNICATION DE CRISE CYBER"
+    },
+    "anssi-guide-attaques_par_rancongiciels_tous_concernes-v1.0.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2020/09/anssi-guide-attaques_par_rancongiciels_tous_concernes-v1.0.pdf",
+        label: "ATTAQUES PAR RANÇONGICIELS, TOUS CONCERNÉS"
+    },
+    "guide_homologation_de_securite_en_9_etapes.pdf": {
+        url: "L’HOMOLOGATION DE SÉCURITÉ",
+        label: "https://cyber.gouv.fr/sites/default/files/2014/06/guide_homologation_de_securite_en_9_etapes.pdf"
+    },
+    "anssi-guide-authentification_multifacteur_et_mots_de_passe.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2021/10/anssi-guide-authentification_multifacteur_et_mots_de_passe.pdf",
+        label: "RECOMMANDATIONS RELATIVES À L'AUTHENTIFICATION MULTIFACTEUR ET AUX MOTS DE PASSE"
+    },
+    "guide_protection_des_systemes_essentiels.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2020/12/guide_protection_des_systemes_essentiels.pdf",
+        label: "RECOMMANDATIONS POUR LA PROTECTION DES SYSTÈMES D'INFORMATION ESSENTIELS"
+    },
+    "guide-securite-numerique-agile-anssi-pa-v1.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2018/11/guide-securite-numerique-agile-anssi-pa-v1.pdf",
+        label: "AGILITÉ & SÉCURITÉ NUMÉRIQUES"
+    },
+    "anssi-guide-organiser-un-exercice-de-gestion-de-crise-cyber-v1.0.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2020/10/anssi-guide-organiser-un-exercice-de-gestion-de-crise-cyber-v1.0.pdf",
+        label: "ORGANISER UN EXERCICE DE GESTION DE CRISE CYBER"
+    },
+    "anssi-guide-gestion_crise_cyber.pdf": {
+        url: "https://cyber.gouv.fr/sites/default/files/2021/12/anssi-guide-gestion_crise_cyber.pdf",
+        label: "CRISE D’ORIGINE CYBER"
+    }
+}
