78b7d016bf8beb435b1df62b937362cd5152e94b
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-09-03 16:44:43 +0200

    Ajoute la route recherche
    * ajoute la méthode recherche_paragraphes à ClientAlbert
    * modification du nom des routes pour uniformiser la langue de rédaction
    * mise à jour du readme avec un example d'utilisation

--- src/client_albert.py
+++ src/client_albert.py
@@ -1,8 +1,23 @@
+import requests
 from typing import Optional
-from config import ALBERT_API_KEY, BASE_URL_ALBERT
+from config import ALBERT_API_KEY, BASE_URL_ALBERT, COLLECTION_ID_ANSSI_LAB
 
 
 class ClientAlbert:
     def __init__(self) -> None:
         self.base_url: str = BASE_URL_ALBERT
         self.api_key: Optional[str] = ALBERT_API_KEY
+
+    def recherche_paragraphes(self, question: str) -> str:
+        session: requests.Session = requests.session()
+        session.headers = {"Authorization": f"Bearer {self.api_key}"}
+        payload = {
+            "collections": [COLLECTION_ID_ANSSI_LAB],
+            "k": 5,
+            "prompt": question,
+            "method": "semantic",
+        }
+        response: requests.Response = session.post(
+            f"{self.base_url}/search", json=payload
+        )
+        return response.text
--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from typing import Dict
 from client_albert import ClientAlbert
+from schemas.requetes import QuestionRequete
 
 app: FastAPI = FastAPI()
+
+
+def fabrique_client_albert() -> ClientAlbert:
+    return ClientAlbert()
 
 
 @app.get("/health")
 def route_sante() -> Dict[str, str]:
     return {"status": "ok"}
+
+
+@app.post("/recherche")
+def route_recherche(
+    request: QuestionRequete,
+    client_albert: ClientAlbert = Depends(fabrique_client_albert),
+) -> str:
+    return client_albert.recherche_paragraphes(request.question)
