16e64944c546796d871b59f29beb3d4341e9983d
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-09-09 10:22:15 +0200

    [SOIN] Modifie dans quel fichier est défini COLLECTION_ID_ANSSI_LAB
    ...pour permettre de le définir dans les variables d'environnement
    et non dans le code `config.py`.

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 import os
 
 BASE_URL_ALBERT: str = "https://albert.api.etalab.gouv.fr/v1"
-COLLECTION_ID_ANSSI_LAB: int = 1954
 COLLECTION_NOM_ANSSI_LAB: str = "ANSSI_test"
 MODELE_REPONSE_ALBERT: str = "albert-large"
 
 HOST: str = os.getenv("HOST", "127.0.0.1")
 PORT: int = int(os.getenv("PORT", "8000"))
 
+COLLECTION_ID_ANSSI_LAB: int = int(os.getenv("COLLECTION_ID_ANSSI_LAB"))
 ALBERT_API_KEY: str = os.getenv("ALBERT_API_KEY")
 assert ALBERT_API_KEY, "ALBERT_API_KEY doit être définie"
