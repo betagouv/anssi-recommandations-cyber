c1836242d9c2ea56db1e7b25b9b90322d2cf2682
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-09-09 11:32:12 +0200

    Modifie le modèle ReponseQuestion pour ajouter un Paragraphe
    ... pour faciliter la récupération des informations score, url, page,
    etc.
    * Ajoute un modèle Paragraphe
    * Modifie les tests en conséquences

--- src/schemas/reponses.py
+++ src/schemas/reponses.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel
+from typing import List
+
+
+class Paragraphe(BaseModel):
+    score_similarite: float
+    numero_page: int
+    url: str
+    nom_document: str
+    contenu: str
 
 
 class ReponseQuestion(BaseModel):
     reponse: str
-    paragraphes: str
+    paragraphes: List[Paragraphe]
     question: str
