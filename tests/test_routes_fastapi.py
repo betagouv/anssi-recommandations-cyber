202739986b86df454483d7ba9950db53d7ff84ee
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-09-09 11:21:02 +0200

    Modifie le retour de la route /recherche
    ... pour ajouter pour chaque paragraphes de document, le score, le nom,
    l'url et la page

--- tests/test_routes_fastapi.py
+++ tests/test_routes_fastapi.py
@@ -18,7 +18,7 @@ def test_route_sante() -> None:
 def test_route_recherche_repond_correctement() -> None:
     """Vérifie que l'endpoint recherche fonctionne"""
     mock_client: Mock = Mock(spec=ClientAlbert)
-    mock_client.recherche_paragraphes.return_value = "Réponse de test d'Albert"
+    mock_client.recherche_paragraphes.return_value = {"paragraphs": []}
 
     app.dependency_overrides[fabrique_client_albert] = lambda: mock_client
     try:
@@ -35,14 +35,15 @@ def test_route_recherche_repond_correctement() -> None:
 def test_route_recherche_donnees_correctes() -> None:
     """Vérifie que l'endpoint recherche fonctionne"""
     mock_client: Mock = Mock(spec=ClientAlbert)
-    mock_client.recherche_paragraphes.return_value = "Réponse de test d'Albert"
+    mock_client.recherche_paragraphes.return_value = {"paragraphs": []}
 
     app.dependency_overrides[fabrique_client_albert] = lambda: mock_client
     try:
         client: TestClient = TestClient(app)
         response = client.post("/recherche", json={"question": "Ma question test"})
         resultat = response.json()
-        assert isinstance(resultat, str)
+        assert isinstance(resultat, dict)
+        assert "paragraphs" in resultat
 
         mock_client.recherche_paragraphes.assert_called_once()
 
@@ -100,5 +101,48 @@ def test_route_pose_question_retourne_donnees_correctes() -> None:
         assert resultat["question"] == "Qui es-tu"
 
         mock_client.pose_question.assert_called_once()
+    finally:
+        app.dependency_overrides.clear()
+
+
+def test_route_recherche_retourne_la_bonne_structure_d_objet() -> None:
+    """Vérifie que l'endpoint recherche retourne toutes les métadonnées pour chaque paragraphe"""
+    mock_result = {
+        "paragraphs": [
+            {
+                "content": "Contenu du paragraphe 1",
+                "source_url": "https://cyber.gouv.fr/sites/default/files/2021/10/anssi-guide-authentification_multifacteur_et_mots_de_passe.pdf",
+                "score": 0.75,
+                "page": 29,
+                "document_name": "anssi-guide-authentification_multifacteur_et_mots_de_passe.pdf",
+            },
+        ]
+    }
+
+    mock_client: Mock = Mock(spec=ClientAlbert)
+    mock_client.recherche_paragraphes.return_value = mock_result
+
+    app.dependency_overrides[fabrique_client_albert] = lambda: mock_client
+    try:
+        client: TestClient = TestClient(app)
+        response = client.post("/recherche", json={"question": "Ma question test"})
+
+        resultat = response.json()
+        assert "paragraphs" in resultat
+        assert len(resultat["paragraphs"]) == 1
+        p1 = resultat["paragraphs"][0]
+        assert p1["content"] == "Contenu du paragraphe 1"
+        assert (
+            p1["source_url"]
+            == "https://cyber.gouv.fr/sites/default/files/2021/10/anssi-guide-authentification_multifacteur_et_mots_de_passe.pdf"
+        )
+        assert p1["score"] == 0.75
+        assert p1["page"] == 29
+        assert (
+            p1["document_name"]
+            == "anssi-guide-authentification_multifacteur_et_mots_de_passe.pdf"
+        )
+
+        mock_client.recherche_paragraphes.assert_called_once()
     finally:
         app.dependency_overrides.clear()
