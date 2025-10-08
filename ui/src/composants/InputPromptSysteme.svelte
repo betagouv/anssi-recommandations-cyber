fad42cfc8ba5d6e8400d5cae368ec058ebbc4ab6
Xavier Maso <xavier.maso@beta.gouv.fr>
2025-10-08 16:10:39 +0200

    [UI] Ajoute la possibilité de changer le prompt système
    ... pour faciliter les itérations tant que l'on construit ce service.

---
+++ ui/src/composants/InputPromptSysteme.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let { prompt = $bindable() }: { prompt: string } = $props();
+</script>
+
+<dsfr-textarea value={prompt} rows=10 onvaluechanged={(e: CustomEvent<string>) => prompt = e.detail}></dsfr-textarea>
+
+<style>
+  dsfr-textarea {
+    margin-top: 24px;
+  }
+</style>
