902f61470a4bb76cb1996a517c67f74992d2305f
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-08 19:22:49 +0200

    [SOIN] Extrais la gestion de la conversation dans un store dédié

---
+++ ui/src/stores/conversation.store.ts
@@ -0,0 +1,53 @@
+import { writable } from "svelte/store";
+import DOMPurify from "dompurify";
+import { marked } from "marked";
+
+export type Paragraphe = {
+  score_similarite: number;
+  numero_page: number;
+  url: string;
+  nom_document: string;
+  contenu: string;
+};
+
+export type Message = {
+  contenu: string;
+  emetteur: "utilisateur" | "systeme";
+  references?: Paragraphe[];
+  idInteraction?: string;
+};
+
+const { subscribe, update } = writable<Message[]>([]);
+
+export const storeConversation = {
+  ajouteMessageUtilisateur: (question: string) => {
+    update((messages) => {
+      return [
+        ...messages,
+        {
+          contenu: question,
+          emetteur: "utilisateur",
+        },
+      ];
+    });
+  },
+  ajouteMessageSysteme: async (
+    reponse: string,
+    references: Paragraphe[],
+    idInteraction: string,
+  ) => {
+    const contenuHTML = DOMPurify.sanitize(await marked.parse(reponse));
+    update((messages) => {
+      return [
+        ...messages,
+        {
+          contenu: contenuHTML,
+          emetteur: "systeme",
+          references,
+          idInteraction,
+        },
+      ];
+    });
+  },
+  subscribe,
+};
