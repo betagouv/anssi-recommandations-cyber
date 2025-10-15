4ef106a0e3f279e7dc7c01f271126b38fdf135d5
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-15 09:46:04 +0200

    [UI] Ajoute le header du DSFR

--- ui/src/composants/Entete.svelte
+++ ui/src/composants/Entete.svelte
@@ -1,3 +1,30 @@
-<header>
-  <h1>Mes Questions Cyber</h1>
-</header>
+<dsfr-header
+  id="entete"
+  brand-service="MesQuestionsCyber"
+  has-brand-tagline
+  brand-tagline="Innovation ANSSI"
+  brand-logo-title="République <br>Française"
+  brand-link-id="lien-retour-accueil"
+  brand-link-href="/"
+  brand-link-title="Retour à l'accueil du site - MesQuestionsCyber"
+  has-brand-operator
+  brand-operator-alt="Agence nationale de la sécurité des systèmes d'information"
+  brand-operator-src="./images/ANSSI.svg"
+>
+  <dsfr-badge
+    slot="headerbadge"
+    label="Bêta"
+    type="accent"
+    accent="green-emeraude"
+    size="sm"
+    has-icon={false}
+    ellipsis={false}
+  ></dsfr-badge>
+</dsfr-header>
+
+<style lang="scss">
+  :root {
+    --raised-shadow: 0 1px 3px var(--shadow-color);
+    --shadow-color: rgba(0,0,18,.16);
+  }
+</style>
