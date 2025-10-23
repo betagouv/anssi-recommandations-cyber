b712a195c6d86696f80a8bbf0300307200dc7200
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-10-23 16:43:54 +0200

    Extrais un constructeur de serveur de test
    ... permettant d'injecter facilement des dépendances mockées, et construite similairement.

--- tests/mocks/serveur_de_test.py
+++ tests/mocks/serveur_de_test.py
@@ -1,6 +1,7 @@
 from serveur import fabrique_serveur
 from configuration import Mode
 from unittest.mock import Mock
+from services.albert import ServiceAlbert, fabrique_service_albert
 
 
 class ConstructeurServiceAlbert:
@@ -12,4 +13,18 @@ class ConstructeurServiceAlbert:
         return self._mock
 
 
-serveur = fabrique_serveur(Mode.PRODUCTION)
+class ConstructeurServeur:
+    def __init__(self):
+        self._serveur = fabrique_serveur(Mode.PRODUCTION)
+
+    def avec_service_albert(self, service_albert: ServiceAlbert):
+        self._serveur.dependency_overrides[fabrique_service_albert] = (
+            lambda: service_albert
+        )
+        return self
+
+    def construit(self):
+        return self._serveur
+
+
+serveur = ConstructeurServeur().construit()
