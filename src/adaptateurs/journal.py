6d3cc145b0cc679d4bab7bf6e58959a209f73d3b
Xavier Maso <xavier.maso@beta.gouv.fr>
2025-10-23 13:53:54 +0200

    [SOIN] Abstrait l'adaptateur journal
    ... pour préparer une implémentation Postgres de cet adaptateur.

--- src/adaptateurs/journal.py
+++ src/adaptateurs/journal.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from enum import StrEnum
 from typing import Dict
 
@@ -6,10 +7,16 @@ class TypeEvenement(StrEnum):
     INTERACTION_CREEE = "INTERACTION_CREEE"
 
 
-class AdaptateurJournal:
+class AdaptateurJournal(ABC):
+    @abstractmethod
+    def consigne_evenement(self, type: TypeEvenement, donnees: Dict) -> None:
+        pass
+
+
+class AdaptateurJournalMemoire(AdaptateurJournal):
     def consigne_evenement(self, type: TypeEvenement, donnees: Dict) -> None:
         pass
 
 
 def fabrique_adaptateur_journal() -> AdaptateurJournal:
-    return AdaptateurJournal()
+    return AdaptateurJournalMemoire()
