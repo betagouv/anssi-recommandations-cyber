3310a55faee3b643163a6e6dbf43802e72a7285f
Xavier Maso <xavier.maso@beta.gouv.fr>
2025-10-24 10:40:33 +0200

    [SOIN] Utilise l'adaptateur de base de données dans les tests des retours

--- tests/routes/test_toutes_les_autres.py
+++ tests/routes/test_toutes_les_autres.py
@@ -1,9 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import Mock
-from serveur_de_test import (
-    serveur,
-)
 
 from adaptateurs.chiffrement import (
     AdaptateurChiffrement,
@@ -14,12 +11,8 @@ from adaptateurs.journal import (
     TypeEvenement,
     fabrique_adaptateur_journal,
 )
-from adaptateurs.adaptateur_base_de_donnees_postgres import (
-    fabrique_adaptateur_base_de_donnees_retour_utilisatrice,
-)
 from schemas.client_albert import Paragraphe, ReponseQuestion
 from schemas.retour_utilisatrice import RetourPositif, TagPositif
-from adaptateurs import AdaptateurBaseDeDonnees
 from configuration import Mode
 
 from serveur_de_test import (
@@ -269,87 +262,83 @@ def test_route_retour_avec_mock_retourne_succes_200() -> None:
 
 
 def test_route_retour_avec_mock_retourne_donnees_attendues() -> None:
-    mock_service = Mock(spec=AdaptateurBaseDeDonnees)
-    mock_service.ajoute_retour_utilisatrice.return_value = RetourPositif(
+    retour = RetourPositif(
         commentaire="Très utile !",
         tags=[TagPositif.Complete, TagPositif.FacileAComprendre],
     )
+    adaptateur_base_de_donnees = (
+        ConstructeurAdaptateurBaseDeDonnees().avec_retour(retour).construit()
+    )
+    serveur = (
+        ConstructeurServeur()
+        .avec_adaptateur_base_de_donnees(adaptateur_base_de_donnees)
+        .construit()
+    )
 
-    serveur.dependency_overrides[
-        fabrique_adaptateur_base_de_donnees_retour_utilisatrice
-    ] = lambda: mock_service
+    client = TestClient(serveur)
+    payload = {
+        "id_interaction": "id-123",
+        "retour": {
+            "type": "positif",
+            "commentaire": "Très utile !",
+            "tags": ["complete", "facileacomprendre"],
+        },
+    }
 
-    try:
-        client = TestClient(serveur)
-        payload = {
-            "id_interaction": "id-123",
-            "retour": {
-                "type": "positif",
-                "commentaire": "Très utile !",
-                "tags": ["complete", "facileacomprendre"],
-            },
-        }
+    reponse = client.post("/api/retour", json=payload)
+    data = reponse.json()
 
-        reponse = client.post("/api/retour", json=payload)
-        data = reponse.json()
-
-        assert data["commentaire"] == "Très utile !"
-        assert data["tags"] == [TagPositif.Complete, TagPositif.FacileAComprendre]
-        args, _ = mock_service.ajoute_retour_utilisatrice.call_args
-    finally:
-        serveur.dependency_overrides.clear()
+    assert data["commentaire"] == "Très utile !"
+    assert data["tags"] == [TagPositif.Complete, TagPositif.FacileAComprendre]
+    args, _ = adaptateur_base_de_donnees.ajoute_retour_utilisatrice.call_args
 
 
 def test_route_retour_avec_interaction_inexistante_retourne_404() -> None:
-    mock_service = Mock(spec=AdaptateurBaseDeDonnees)
-    mock_service.ajoute_retour_utilisatrice.return_value = None
+    adaptateur_base_de_donnees = (
+        ConstructeurAdaptateurBaseDeDonnees().avec_retour(None).construit()
+    )
+    serveur = (
+        ConstructeurServeur()
+        .avec_adaptateur_base_de_donnees(adaptateur_base_de_donnees)
+        .construit()
+    )
 
-    serveur.dependency_overrides[
-        fabrique_adaptateur_base_de_donnees_retour_utilisatrice
-    ] = lambda: mock_service
+    client = TestClient(serveur)
+    payload = {
+        "id_interaction": "id-123",
+        "retour": {
+            "type": "positif",
+            "commentaire": "Très utile",
+        },
+    }
 
-    try:
-        client = TestClient(serveur)
-        payload = {
-            "id_interaction": "id-123",
-            "retour": {
-                "type": "positif",
-                "commentaire": "Très utile",
-            },
-        }
+    resp = client.post("/api/retour", json=payload)
 
-        resp = client.post("/api/retour", json=payload)
-
-        assert resp.status_code == 404
-        assert resp.json() == {"detail": "Interaction non trouvée"}
-
-        mock_service.ajoute_retour_utilisatrice.assert_called_once()
-
-    finally:
-        serveur.dependency_overrides.clear()
+    assert resp.status_code == 404
+    assert resp.json() == {"detail": "Interaction non trouvée"}
+    adaptateur_base_de_donnees.ajoute_retour_utilisatrice.assert_called_once()
 
 
 def test_route_retour_avec_payload_invalide_rejette_la_requete() -> None:
-    mock_service = Mock(spec=AdaptateurBaseDeDonnees)
-    serveur.dependency_overrides[
-        fabrique_adaptateur_base_de_donnees_retour_utilisatrice
-    ] = lambda: mock_service
+    adaptateur_base_de_donnees = ConstructeurAdaptateurBaseDeDonnees().construit()
+    serveur = (
+        ConstructeurServeur()
+        .avec_adaptateur_base_de_donnees(adaptateur_base_de_donnees)
+        .construit()
+    )
 
-    try:
-        client = TestClient(serveur)
-        payload = {
-            "id": 23,
-            "retour": {
-                "type": "positif",
-                "commentaire": "Très utile",
-            },
-        }
-        resp = client.post("/api/retour", json=payload)
+    client = TestClient(serveur)
+    payload = {
+        "id": 23,
+        "retour": {
+            "type": "positif",
+            "commentaire": "Très utile",
+        },
+    }
+    resp = client.post("/api/retour", json=payload)
 
-        assert resp.status_code == 422
-        mock_service.ajoute_retour_utilisatrice.assert_not_called()
-    finally:
-        serveur.dependency_overrides.clear()
+    assert resp.status_code == 422
+    adaptateur_base_de_donnees.ajoute_retour_utilisatrice.assert_not_called()
 
 
 def test_pose_question_retourne_id_dans_body() -> None:
