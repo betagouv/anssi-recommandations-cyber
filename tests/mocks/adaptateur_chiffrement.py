acc0595e5178066e257b22f3ae88c95f8ec52f0c
Xavier Maso <xavier.maso@beta.gouv.fr>
2025-10-28 14:26:53 +0100

    Isole le constructeur d'adaptateur de chiffrement
    ... pour que cela soit moins confusant à l'utilisation dans d'autres
    tests.

---
+++ tests/mocks/adaptateur_chiffrement.py
@@ -0,0 +1,17 @@
+from unittest.mock import Mock
+
+
+class ConstructeurAdaptateurChiffrement:
+    def __init__(self):
+        self._mock = Mock()
+
+    def qui_hache(self, hache: str):
+        self._mock.hache.return_value = hache
+        return self
+
+    def qui_retourne_nonce(self, nonce: str):
+        self._mock.recupere_nonce.return_value = nonce
+        return self
+
+    def construit(self):
+        return self._mock
