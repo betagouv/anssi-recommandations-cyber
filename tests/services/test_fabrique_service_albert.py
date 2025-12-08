aadb6de11e5309f2d60f22b2bcf61c97109dad5b
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-12-08 14:51:10 +0100

    [SOIN] Déplace les tests de fabrique

---
+++ tests/services/test_fabrique_service_albert.py
@@ -0,0 +1,12 @@
+from infra.albert.client_albert import ClientAlbertApi
+from services.fabrique_service_albert import fabrique_service_albert
+
+
+def test_peut_fabriquer_un_service_albert_avec_une_configuration_par_defaut() -> None:
+    service_albert = fabrique_service_albert()
+
+    assert isinstance(service_albert.client, ClientAlbertApi)
+    assert (
+        "Tu es un service développé par ou pour l’ANSSI"
+        in service_albert.PROMPT_SYSTEME
+    )
