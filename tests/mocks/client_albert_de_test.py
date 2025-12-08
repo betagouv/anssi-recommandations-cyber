99fadeeedb124e770a1e09942737a0da5f2eb569
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-12-08 17:20:35 +0100

    [SOIN] Sépare les tests de service des tests de l'API
    ... Et ajoute un ClientAlbertMemoire pour faciliter les tests

--- tests/mocks/client_albert_de_test.py
+++ tests/mocks/client_albert_de_test.py
@@ -2,12 +2,20 @@ import requests
 from unittest.mock import Mock
 
 from openai import APITimeoutError, OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+from openai.types.chat.chat_completion import Choice, ChatCompletionMessage
 
 from configuration import Albert
 from infra.albert.client_albert import (
     ClientAlbertApi,
 )
-from services.service_albert import ServiceAlbert
+from schemas.albert import (
+    RecherchePayload,
+    ResultatRecherche,
+    RechercheChunk,
+    RechercheMetadonnees,
+)
+from services.service_albert import ServiceAlbert, ClientAlbert
 
 
 class RetourRouteSearch:
@@ -142,3 +150,77 @@ class ConstructeurServiceAlbert:
             prompt_systeme=self.PROMPT_SYSTEME_ALTERNATIF,
             utilise_recherche_hybride=self.FAUSSE_CONFIGURATION_ALBERT_CLIENT.utilise_recherche_hybride,
         )
+
+
+class ClientAlbertMemoire(ClientAlbert):
+    def __init__(self):
+        self.propositions_vides = False
+        self.resultats_vides = False
+        self.messages_recus = []
+        self.payload_recu = None
+        self.resultats = []
+        self.choix = []
+
+    def recherche(self, payload: RecherchePayload) -> list[ResultatRecherche]:
+        self.payload_recu = payload
+        return self.resultats if self.resultats_vides is False else []
+
+    def recupere_propositions(
+        self, messages: list[ChatCompletionMessageParam]
+    ) -> list[Choice]:
+        self.messages_recus = messages
+        return self.choix if self.propositions_vides is False else []
+
+    def avec_les_resultats(self, resultats: list[ResultatRecherche]):
+        self.resultats.extend(resultats)
+
+    def avec_les_propositions(self, choix: list[Choice]):
+        self.choix.extend(choix)
+
+    def sans_resultats(self):
+        self.resultats_vides = True
+
+    def sans_propositions(self):
+        self.propositions_vides = True
+
+
+class ConstructeurResultatDeRecherche:
+    def __init__(self):
+        self.contenu = "Un contenu"
+
+    def ayant_pour_contenu(self, contenu: str):
+        self.contenu = contenu
+        return self
+
+    def construis(self) -> ResultatRecherche:
+        return ResultatRecherche(
+            RechercheChunk(
+                content=self.contenu,
+                metadata=RechercheMetadonnees(source_url="", page=1, nom_document=""),
+            ),
+            0.5,
+        )
+
+
+class ConstructeurDeChoix:
+    def __init__(self):
+        self.contenu = "Un contenu"
+
+    def ayant_pour_contenu(self, contenu: str):
+        self.contenu = contenu
+        return self
+
+    def construis(self) -> Choice:
+        return Choice(
+            message=ChatCompletionMessage(content=self.contenu, role="assistant"),
+            finish_reason="stop",
+            index=1,
+        )
+
+
+def un_resultat_de_recherche() -> ConstructeurResultatDeRecherche:
+    return ConstructeurResultatDeRecherche()
+
+
+def un_choix_de_proposition() -> ConstructeurDeChoix:
+    return ConstructeurDeChoix()
