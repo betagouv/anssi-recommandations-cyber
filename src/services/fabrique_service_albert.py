bc329fd2dd4322335dbbe35022a2b61e6b98d0c7
pie-leroy <pierre.leroy@beta.gouv.fr>
2025-12-09 15:22:46 +0100

    Lis le prompt de reclassement

--- src/services/fabrique_service_albert.py
+++ src/services/fabrique_service_albert.py
@@ -9,13 +9,19 @@ def fabrique_service_albert() -> ServiceAlbert:
     configuration = recupere_configuration()
 
     client_albert_api = fabrique_client_albert(configuration.albert.client)
-
-    template_path = Path.cwd() / "templates" / "prompt_assistant_cyber.txt"
-    prompt_systeme: str = template_path.read_text(encoding="utf-8")
+    prompt_systeme = lis_fichier_prompt("prompt_assistant_cyber.txt")
+    prompt_reclassement = lis_fichier_prompt("prompt_reclassement.txt")
 
     return ServiceAlbert(
         configuration_service_albert=configuration.albert.service,
         client=client_albert_api,
         utilise_recherche_hybride=configuration.albert.client.utilise_recherche_hybride,
-        prompts=Prompts(prompt_systeme=prompt_systeme, prompt_reclassement=""),
+        prompts=Prompts(
+            prompt_systeme=prompt_systeme, prompt_reclassement=prompt_reclassement
+        ),
     )
+
+
+def lis_fichier_prompt(nom_fichier_prompt: str) -> str:
+    template_path = Path.cwd() / "templates" / nom_fichier_prompt
+    return template_path.read_text(encoding="utf-8")
