237474ac0fbcd9bd83770fe9c8f203133cd0ac0e
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-12-11 15:29:35 +0100

    Initialise un service de chiffrement AES
    … suivant la présence de la variable d’environnement CHIFFREMENT_CLEF_DE_CHIFFREMENT

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import uvicorn
+
 from adaptateurs.chiffrement import fabrique_adaptateur_chiffrement
 from configuration import recupere_configuration
 from infra.chiffrement.chiffrement import (
-    ServiceDeChiffrement,
-    FournisseurDeServiceDeChiffrement,
+    fabrique_fournisseur_de_chiffrement,
 )
 from serveur import fabrique_serveur
 
@@ -12,17 +12,7 @@ adaptateur_chiffrement = fabrique_adaptateur_chiffrement()
 serveur = fabrique_serveur(
     configuration.max_requetes_par_minute, configuration.mode, adaptateur_chiffrement
 )
-
-
-class ServiceDeChiffrementDeTest(ServiceDeChiffrement):
-    def dechiffre(self, contenu_chiffre: str) -> str:
-        return contenu_chiffre.removesuffix("_chiffre")
-
-    def chiffre(self, contenu: str) -> str:
-        return f"{contenu}_chiffre"  # type: ignore
-
-
-FournisseurDeServiceDeChiffrement.service = ServiceDeChiffrementDeTest()
+fabrique_fournisseur_de_chiffrement(configuration)
 
 if __name__ == "__main__":
     HOST = configuration.hote
