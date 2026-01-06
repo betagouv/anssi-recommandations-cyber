38126596530878a7b9bf8e1809b2f1c68d0b3eef
pie-leroy <pierre.leroy@beta.gouv.fr>
2026-01-06 10:28:32 +0100

    Injecte le service de chiffrement dans l'adaptateur de chiffrement

--- src/adaptateurs/chiffrement.py
+++ src/adaptateurs/chiffrement.py
@@ -2,11 +2,18 @@ import hashlib
 import secrets
 from abc import ABC, abstractmethod
 from configuration import Chiffrement, recupere_configuration
+from infra.chiffrement.chiffrement import (
+    fabrique_fournisseur_de_chiffrement,
+    ServiceDeChiffrement,
+)
 
 
 class AdaptateurChiffrement(ABC):
-    def __init__(self, configuration: Chiffrement):
+    def __init__(
+        self, configuration: Chiffrement, service_de_chiffrement: ServiceDeChiffrement
+    ):
         self.sel_de_hachage = configuration.sel_de_hachage
+        self.service_de_chiffrement = service_de_chiffrement
 
     @abstractmethod
     def hache(self, valeur: str) -> str:
@@ -16,8 +23,22 @@ class AdaptateurChiffrement(ABC):
     def recupere_nonce(self) -> str:
         pass
 
+    @abstractmethod
+    def chiffre(self, contenu: str) -> str:
+        pass
+
+    @abstractmethod
+    def dechiffre(self, contenu_chiffre: str) -> str:
+        pass
+
 
 class AdaptateurChiffrementStandard(AdaptateurChiffrement):
+    def chiffre(self, contenu: str) -> str:
+        return self.service_de_chiffrement.chiffre(contenu)
+
+    def dechiffre(self, contenu_chiffre: str) -> str:
+        return self.service_de_chiffrement.dechiffre(contenu_chiffre)
+
     def hache(self, valeur: str) -> str:
         return hashlib.sha256(f"{self.sel_de_hachage}{valeur}".encode()).hexdigest()
 
@@ -27,4 +48,7 @@ class AdaptateurChiffrementStandard(AdaptateurChiffrement):
 
 def fabrique_adaptateur_chiffrement() -> AdaptateurChiffrement:
     configuration = recupere_configuration().chiffrement
-    return AdaptateurChiffrementStandard(configuration)
+    service_de_chiffrement = fabrique_fournisseur_de_chiffrement(
+        recupere_configuration()
+    )
+    return AdaptateurChiffrementStandard(configuration, service_de_chiffrement)
