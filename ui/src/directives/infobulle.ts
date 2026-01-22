44f13c767a91c9d5140ab65d219fc0d5310c8a48
pie-leroy <pierre.leroy@beta.gouv.fr>
2026-01-22 13:55:30 +0100

    Ajoute l'infobulle notifiant que le message a été copié

--- ui/src/directives/infobulle.ts
+++ ui/src/directives/infobulle.ts
@@ -1,9 +1,73 @@
-export const infobulle = (noeud: HTMLElement, contenu: string) => {
+export type PropsInfoBulle = {
+  contenu: string;
+  mode: "click" | "mouse";
+};
+
+const contenuEstUneChaine = (
+  contenu: string | PropsInfoBulle,
+): contenu is string => typeof contenu === "string";
+const contenuEstUneProps = (
+  contenu: string | PropsInfoBulle,
+): contenu is PropsInfoBulle =>
+  typeof contenu === "object" && "contenu" in contenu && "mode" in contenu;
+
+type Listeners = {
+  [key: string]: (noeud: HTMLElement) => {
+    ajoute: () => void;
+    supprime: () => void;
+  };
+};
+
+export const infobulle = (
+  noeud: HTMLElement,
+  contenu: string | PropsInfoBulle,
+) => {
+  const listeners: Listeners = {
+    mouse: (noeud: HTMLElement) => {
+      return {
+        ajoute: () => {
+          noeud.addEventListener("pointerenter", surEntree);
+          noeud.addEventListener("pointerleave", surSortie);
+          noeud.addEventListener("pointerdown", surTapNoeud);
+          document.addEventListener("pointerdown", surTapAilleurs, {
+            passive: true,
+          });
+          window.addEventListener("scroll", surScrollOuBlur, { passive: true });
+          window.addEventListener("blur", surScrollOuBlur);
+          window.addEventListener("pointercancel", surCancel);
+        },
+        supprime: () => {
+          noeud.removeEventListener("pointerenter", surEntree);
+          noeud.removeEventListener("pointerleave", surSortie);
+          noeud.removeEventListener("pointerdown", surTapNoeud);
+          document.removeEventListener("pointerdown", surTapAilleurs);
+          window.removeEventListener("scroll", surScrollOuBlur);
+          window.removeEventListener("blur", surScrollOuBlur);
+          window.removeEventListener("pointercancel", surCancel);
+        },
+      };
+    },
+    click: (noeud: HTMLElement) => {
+      return {
+        ajoute: () => {
+          noeud.addEventListener("click", surClick);
+        },
+        supprime: () => {
+          noeud.removeEventListener("click", surClick);
+        },
+      };
+    },
+  };
+
   const elementInfobulle = document.createElement("div");
   const elementContenuInfobulle = document.createElement("p");
   elementInfobulle.classList.add("conteneur-infobulle");
-  elementContenuInfobulle.textContent = contenu;
+  elementContenuInfobulle.textContent = contenuEstUneChaine(contenu)
+    ? contenu
+    : contenu.contenu;
+  const mode = contenuEstUneProps(contenu) ? contenu.mode : "mouse";
   elementInfobulle.appendChild(elementContenuInfobulle);
+
   document.body.appendChild(elementInfobulle);
 
   const estVisible = () => elementInfobulle.style.display === "flex";
@@ -27,6 +91,12 @@ export const infobulle = (noeud: HTMLElement, contenu: string) => {
   const surEntree = (e: PointerEvent) => {
     if (e.pointerType === "mouse") affiche();
   };
+
+  const surClick = () => {
+    affiche();
+    setTimeout(() => masque(), 2000);
+  };
+
   const surSortie = (e: PointerEvent) => {
     if (e.pointerType === "mouse") masque();
   };
@@ -45,26 +115,11 @@ export const infobulle = (noeud: HTMLElement, contenu: string) => {
   const surScrollOuBlur = () => masque();
   const surCancel = () => masque();
 
-  noeud.addEventListener("pointerenter", surEntree);
-  noeud.addEventListener("pointerleave", surSortie);
-  noeud.addEventListener("pointerdown", surTapNoeud);
-
-  document.addEventListener("pointerdown", surTapAilleurs, { passive: true });
-  window.addEventListener("scroll", surScrollOuBlur, { passive: true });
-  window.addEventListener("blur", surScrollOuBlur);
-  window.addEventListener("pointercancel", surCancel);
+  listeners[mode](noeud).ajoute();
 
   return {
     destroy() {
-      noeud.removeEventListener("pointerenter", surEntree);
-      noeud.removeEventListener("pointerleave", surSortie);
-      noeud.removeEventListener("pointerdown", surTapNoeud);
-
-      document.removeEventListener("pointerdown", surTapAilleurs);
-      window.removeEventListener("scroll", surScrollOuBlur);
-      window.removeEventListener("blur", surScrollOuBlur);
-      window.removeEventListener("pointercancel", surCancel);
-
+      listeners[mode](noeud).supprime();
       elementInfobulle.remove();
     },
   };
