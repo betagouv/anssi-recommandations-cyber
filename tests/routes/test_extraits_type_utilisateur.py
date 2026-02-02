082914161710c54d5a1f16c0431592e5b851b255
Pierre Leroy <pie.leroy@hotmail.com>
2026-02-02 11:34:33 +0100

    Corrige l'interprétation du type_utilisateur dans pose_question
    ... le type utilisateur n'est pas encodé et est donc mal interprété.

---
+++ tests/routes/test_extraits_type_utilisateur.py
@@ -0,0 +1,11 @@
+from serveur import extrais_type_utilisateur
+from adaptateur_chiffrement import AdaptateurChiffrementDeTest
+
+
+def test_extraits_type_utilisateur():
+    adaptateur_chiffrement = AdaptateurChiffrementDeTest()
+    type_utilisateur = "jOrtest%20=="
+
+    extrais_type_utilisateur(adaptateur_chiffrement, type_utilisateur)
+
+    assert adaptateur_chiffrement.contenu_recu == "jOrtest+=="
