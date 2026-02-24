825c0765b8c4c33e66913bee886074eee138bc13
Pierre Leroy <pie.leroy@hotmail.com>
2026-02-24 16:36:51 +0100

    En cas d'erreur, récupère le message d'erreur du back depuis le front,
    ... pour propager le message d'erreur.

--- ui/src/stores/affichage.store.ts
+++ ui/src/stores/affichage.store.ts
@@ -4,11 +4,13 @@ import { tick } from 'svelte';
 type EtatAffichage = {
   enAttenteDeReponse: boolean;
   aUneErreurAlbert: boolean;
+  messageErreurAlbert: string;
 };
 
 const etatAffichageParDefaut: EtatAffichage = {
   enAttenteDeReponse: false,
   aUneErreurAlbert: false,
+  messageErreurAlbert: '',
 };
 
 const { subscribe, update } = writable<EtatAffichage>(etatAffichageParDefaut);
@@ -17,8 +19,12 @@ export const storeAffichage = {
   estEnAttenteDeReponse: (etat: boolean) => {
     update((etatActuel) => ({ ...etatActuel, enAttenteDeReponse: etat }));
   },
-  erreurAlbert: (estEnErreur: boolean) => {
-    update((etatActuel) => ({ ...etatActuel, aUneErreurAlbert: estEnErreur }));
+  erreurAlbert: (estEnErreur: boolean, message: string = '') => {
+    update((etatActuel) => ({
+      ...etatActuel,
+      aUneErreurAlbert: estEnErreur,
+      messageErreurAlbert: estEnErreur ? message : '',
+    }));
   },
   scrollVersDernierMessage: async () => {
     await tick();
