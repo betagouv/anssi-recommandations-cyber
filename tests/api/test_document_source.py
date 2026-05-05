97f2c989c2ca47d931428124bbc95010369be9ac
Pierre Leroy <pie.leroy@hotmail.com>
2026-05-05 17:31:11 +0200

    Renvoie des erreurs 404 si pas le bon document ou le numéro de page

--- tests/api/test_document_source.py
+++ tests/api/test_document_source.py
@@ -94,3 +94,61 @@ def test_retourne_une_erreur_404_si_l_interaction_n_est_pas_trouvee(
     )
 
     assert reponse.status_code == 404
+
+
+def test_genere_une_erreur_404_si_le_document_n_est_pas_trouve(
+    un_serveur_de_test, un_constructeur_d_interaction, un_constructeur_de_paragraphe
+):
+    adaptateur_base_de_donnees = AdaptateurBaseDeDonneesEnMemoire("id-interaction-test")
+    serveur = un_serveur_de_test(
+        adaptateur_base_de_donnees=adaptateur_base_de_donnees,
+        adaptateur_journal=AdaptateurJournalMemoire(),
+    )
+    paragraphe = (
+        un_constructeur_de_paragraphe()
+        .a_la_page(30)
+        .dans_le_document("anssi-guide-gestion_crise_cyber.pdf")
+        .construis()
+    )
+    une_interaction = (
+        un_constructeur_d_interaction()
+        .avec_une_reponse_contenant_les_paragraphes([paragraphe])
+        .construis()
+    )
+    adaptateur_base_de_donnees.sauvegarde_interaction(une_interaction)
+    client_http = TestClient(serveur, follow_redirects=False)
+
+    reponse = client_http.get(
+        f"/source/?document=un_document_inconnu.pdf&page=42&interaction={str(une_interaction.id)}"
+    )
+
+    assert reponse.status_code == 404
+
+
+def test_retourne_erreur_404_si_le_numero_de_page_ne_correspond_pas(
+    un_serveur_de_test, un_constructeur_d_interaction, un_constructeur_de_paragraphe
+):
+    adaptateur_base_de_donnees = AdaptateurBaseDeDonneesEnMemoire("id-interaction-test")
+    serveur = un_serveur_de_test(
+        adaptateur_base_de_donnees=adaptateur_base_de_donnees,
+        adaptateur_journal=AdaptateurJournalMemoire(),
+    )
+    premier_paragraphe = (
+        un_constructeur_de_paragraphe()
+        .a_la_page(30)
+        .dans_le_document("anssi-guide-gestion_crise_cyber.pdf")
+        .construis()
+    )
+    une_interaction = (
+        un_constructeur_d_interaction()
+        .avec_une_reponse_contenant_les_paragraphes([premier_paragraphe])
+        .construis()
+    )
+    adaptateur_base_de_donnees.sauvegarde_interaction(une_interaction)
+    client_http = TestClient(serveur, follow_redirects=False)
+
+    reponse = client_http.get(
+        f"/source/?document=anssi-guide-gestion_crise_cyber.pdf&page=42&interaction={str(une_interaction.id)}"
+    )
+
+    assert reponse.status_code == 404
