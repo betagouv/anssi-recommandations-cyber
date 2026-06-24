eac43f3cf7e08542939980cc8118960b7bc40944
Pierre Leroy <pierre.leroy@beta.gouv.fr>
2026-06-24 11:19:20 +0200

    Expose une route POST /api/avis pour récupérer les avis utilisateurices

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from typing import Dict
 
+from api.api_avis import api_avis
 from api.api_retour import api_retour
 from api.conversation import api_conversation
 from api.recherche import api_recherche
@@ -9,6 +10,7 @@ api = APIRouter(prefix="/api")
 api.include_router(api_recherche)
 api.include_router(api_conversation)
 api.include_router(api_retour)
+api.include_router(api_avis)
 api_developpement = APIRouter(prefix="/api")
 
 
