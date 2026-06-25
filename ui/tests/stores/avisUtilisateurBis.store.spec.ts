07eda291e5699fbfcb7c998a9fb4a8a88e81d117
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-06-25 11:11:39 +0200

    Ajoute un store pour l’avis utilisateur·rice bis

--- ui/tests/stores/avisUtilisateurBis.store.spec.ts
+++ ui/tests/stores/avisUtilisateurBis.store.spec.ts
@@ -1,25 +1,98 @@
+import { storeAvisUtilisateurBis } from '../../src/stores/avisUtilisateurBis.store';
+import { get } from 'svelte/store';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('le store de conversation', () => {
-
-  afterEach(() => {
+  beforeEach(() => {
     storeAvisUtilisateurBis.initialise({
-      exactitude: {valeur: 'Bonne', commentaire: undefined},
-      completude: {valeur: 'Bonne', commentaire: undefined},
+      exactitude: { valeur: 'Bonne' },
+      completude: { valeur: 'Bonne' },
       idConversation: '123',
       idInteraction: '456',
     });
   });
 
-  it('modifie la valeur de l\'exactitude', () => {
+  it("modifie la valeur de l'exactitude", () => {
+    storeAvisUtilisateurBis.modifieLaValeurDeLExactitude('Très bonne');
 
-      storeAvisUtilisateurBis.modifieLaValeurDeLExactitude('Très bonne');
-      const conversation = get(storeAvisUtilisateurBis);
-      expect(conversation).toStrictEqual({
-      exactitude: {valeur: 'Très bonne', commentaire: undefined},
-      completude: {valeur: 'Bonne', commentaire: undefined},
+    const conversation = get(storeAvisUtilisateurBis);
+    expect(conversation).toStrictEqual({
+      exactitude: { valeur: 'Très bonne' },
+      completude: { valeur: 'Bonne' },
       idConversation: '123',
       idInteraction: '456',
-    })
-
     });
+  });
+
+  it('peut ajouter un commentaire lorsque l’exactitude est différente de fausse', () => {
+    storeAvisUtilisateurBis.commenteLExactitude('La réponse est exacte');
+
+    const conversation = get(storeAvisUtilisateurBis);
+    expect(conversation).toStrictEqual({
+      exactitude: { valeur: 'Bonne', commentaire: 'La réponse est exacte' },
+      completude: { valeur: 'Bonne' },
+      idConversation: '123',
+      idInteraction: '456',
+    });
+  });
+
+  it('ne peut pas ajouter de commentaire lorsque l’exactitude est fausse', () => {
+    storeAvisUtilisateurBis.initialise({
+      exactitude: { valeur: 'Fausse' },
+      completude: { valeur: 'Bonne' },
+      idConversation: '123',
+      idInteraction: '456',
+    });
+    storeAvisUtilisateurBis.commenteLExactitude('La réponse est fausse');
+
+    const conversation = get(storeAvisUtilisateurBis);
+    expect(conversation).toStrictEqual({
+      exactitude: { valeur: 'Fausse' },
+      completude: { valeur: 'Bonne' },
+      idConversation: '123',
+      idInteraction: '456',
+    });
+  });
+
+  it('modifie la valeur de la complétude', () => {
+    storeAvisUtilisateurBis.modifieLaValeurDeLaCompletude('Très bonne');
+
+    const conversation = get(storeAvisUtilisateurBis);
+    expect(conversation).toStrictEqual({
+      exactitude: { valeur: 'Bonne' },
+      completude: { valeur: 'Très bonne' },
+      idConversation: '123',
+      idInteraction: '456',
+    });
+  });
+
+  it('peut ajouter un commentaire lorsque la complétude est différente de fausse', () => {
+    storeAvisUtilisateurBis.commenteLaCompletude('La réponse est exacte');
+
+    const conversation = get(storeAvisUtilisateurBis);
+    expect(conversation).toStrictEqual({
+      exactitude: { valeur: 'Bonne' },
+      completude: { valeur: 'Bonne', commentaire: 'La réponse est exacte' },
+      idConversation: '123',
+      idInteraction: '456',
+    });
+  });
+
+  it('ne peut pas ajouter de commentaire lorsque la complétude est mauvaise', () => {
+    storeAvisUtilisateurBis.initialise({
+      exactitude: { valeur: 'Bonne' },
+      completude: { valeur: 'Mauvaise' },
+      idConversation: '123',
+      idInteraction: '456',
+    });
+    storeAvisUtilisateurBis.commenteLaCompletude('La réponse est fausse');
+
+    const conversation = get(storeAvisUtilisateurBis);
+    expect(conversation).toStrictEqual({
+      exactitude: { valeur: 'Bonne' },
+      completude: { valeur: 'Mauvaise' },
+      idConversation: '123',
+      idInteraction: '456',
+    });
+  });
 });
